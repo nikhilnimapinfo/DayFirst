eda120d1b43f810fed4fcb4888b4bb0e6f0990b9
nikhilnimapinfo <166018854+nikhilnimapinfo@users.noreply.github.com>
2024-04-05 19:14:00 +0530

    Add files via upload

---
+++ Operators.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Type_Conversion
+{
+    class A { }
+    internal class Operators
+    {
+        public void main( )
+        {
+            int a;
+            int b;
+            int c;
+
+
+             a = 10;
+             c = 10;
+            Console.WriteLine(a == c); // true
+
+            b = 20; c = 30;
+            Console.WriteLine(b > c);   //FALSE
+
+            a = 10; b = 20;
+            Console.WriteLine(a != b);  //TRUE
+
+            c = 30; b = 20;
+            Console.WriteLine(c >= b);  //TRUE
+
+            a = 10; c = 30;
+            Console.WriteLine(a <= c);   //TRUE
+
+            a = 10; c = 30;
+            Console.WriteLine(a <= c && a==c);  //FALSE
+
+            a = 10;c = 30;
+            Console.WriteLine(a <= c || a == c); //true
+
+        }   
+    }
+}
---
+++ Type_CONVERSION.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Type_Conversion
+{
+    internal class Type_CONVERSION
+    {
+        static void Main(string[] args)
+        {
+            Operators o = new Operators();
+            o.main();
+
+            // Implicite Type Conversion
+            byte b = 255;
+            int i = b;
+            Console.WriteLine(i);
+
+            //Explicite Type Conversion
+            int k = 1000;
+            byte b1 =(byte) k;
+            Console.WriteLine(b1);
+
+            //Non-Compatable Type
+            var s = "11111111111111111";
+            long i1 = Convert.ToInt64(s);
+            Console.WriteLine(i1);
+
+            try {
+                //To handle an exception
+                var v = "11111";
+                byte b2 = Convert.ToByte(v);
+                Console.WriteLine(b2);
+            }
+            catch
+            {
+                Console.WriteLine("can not convert the value string to byte");
+            }
+
+            string s1 = "true";
+            Boolean bb = Convert.ToBoolean(s1);
+            Console.WriteLine(bb);
+
+        }
+    }
+}
